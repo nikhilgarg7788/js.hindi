40aea0c6e725db34f25a33a7611f905587d58683
nikhilgarg7788 <160724689+nikhilgarg7788@users.noreply.github.com>
2024-12-22 06:11:30 +0000

    new

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,7 +1,10 @@
 const marvel_heros = ["thor", "ironman", "spiderman"]
 const dc_heros = ["supreman", "flash", "batman"]
 
-marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)
 
+//  console.log(marvel_heros);
+//  console.log(marvel_heros[3][1]);
+
+marvel_heros.concat(dc_heros)
 console.log(marvel_heros);
-console.log(marvel_heros)
