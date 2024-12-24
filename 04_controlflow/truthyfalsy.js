3f64f9753c4891f2566444db42145081fb5de0aa
nikhilgarg7788 <160724689+nikhilgarg7788@users.noreply.github.com>
2024-12-24 10:21:48 +0000

    truthy

--- 04_controlflow/truthyfalsy.js
+++ 04_controlflow/truthyfalsy.js
@@ -29,3 +29,26 @@ const emptyObj = {}
 if(Object.keys(emptyObj).length === 0 ){
     console.log("obj is empty");
 }
+
+
+//  NULLISH COALESCING OPERATOR(??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+// we use this operator bcs when we call from database or use
+// firebase or typewriter then we directly get response 
+// we get two values so that our wholecode structure get disturbed
+// for this purpose this is used
+
+console.log(val1);
+
+
+
+// ++++++TERNIARY OPERATOR+++++++++++++++
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80")
