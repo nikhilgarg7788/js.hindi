fe4dc57eca52ffe4bfcfec75fa5fb73ae746514a
nikhilgarg7788 <160724689+nikhilgarg7788@users.noreply.github.com>
2024-12-24 11:15:46 +0000

    loop

---
+++ 05_iterationorloops/three.js
@@ -0,0 +1,46 @@
+//  FOROF LOOP
+
+// ["" , "" ,"" ] strings inside an 
+// [{}, {}, {}] objects inside an Array
+
+const arr = [1, 2, 3, 4, 5]
+
+for(const num of arr){
+    // console.log(num);
+}
+
+const greetings = "Hello World!"
+for(const greet of greetings) {
+    // console.log(`each char is ${greet}`);
+}
+
+
+
+
+// +++++++++++++++++++++ MAPS ++++++++++++++
+// maps are known for unique values no duplicate entry is there
+const map = new Map()
+map.set('IN', "india")
+map.set('USA', "united states of india")
+map.set('Fr', "france")
+map.set('IN', "india")
+
+// console.log(map);
+
+// MAPS using FOROF loop
+
+for(const [key, value] of map){
+    console.log(key, ':-', value);
+}
+
+
+// FOROF loop on objects
+const myObject ={
+    game1: "NFS",
+    game2: "spiderman"
+}
+
+// for(const [key, value] of myObjects){
+//     console.log(key, ':-', value);
+// }
+//  here this structure of FOROF loop doesnt work
