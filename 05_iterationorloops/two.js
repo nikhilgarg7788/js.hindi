65d504a17143ba5a9922fd828046025bef36b4f7
nikhilgarg7788 <160724689+nikhilgarg7788@users.noreply.github.com>
2024-12-24 10:52:54 +0000

    loop

---
+++ 05_iterationorloops/two.js
@@ -0,0 +1,36 @@
+//  WHILE LOOP
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2    
+// }
+
+
+
+// let myArr = ["flash","batman","superman"]
+// let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr = arr + 1
+// }
+
+
+
+
+
+
+//  DO WHILE LOOP
+
+let score = 10
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+let score1 = 11
+do {
+    console.log(`score is ${score1}`);
+    score1++
+} while (score1 <= 10);
+
